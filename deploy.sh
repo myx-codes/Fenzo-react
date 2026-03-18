4077eec10985ef65f7e90be6895b320b1ad786b1
Steven <muhammad95@dgu.ac.kr>
2026-03-18 20:24:15 +0900

    fix: modify

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ git reset --hard
 git checkout main
 git pull origin main
 
-docker compose up -d
+# docker compose up -d
 
-# npm i yarn -g
-# yarn global add serve
-# yarn 
-# yarn run build
-# pm2 start "yarn run start:prod" --name=Fenzo_react
+npm i yarn -g
+yarn global add serve
+yarn 
+yarn run build
+pm2 start "yarn run start:prod" --name=Fenzo_react
